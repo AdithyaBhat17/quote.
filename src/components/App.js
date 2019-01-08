32402ec7ea806c9dd22941f21d4ed1c00cd7311a
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-09 01:49:34 +0530

    ✨ return formatted text to the child component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Quote from './Quote';
+import Quote from './Quote'
+
+import Moon from '../assets/Moon.png'
+import Rocket from '../assets/rocket.png'
+import Blood from '../assets/Blood.png'
 
 class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
             quotes: [],
-            click: 0
+            click: 0,
+            loading: true
         }
         this.fetchQuote = this.fetchQuote.bind(this)
         this.handleClick = this.handleClick.bind(this)
@@ -21,7 +26,9 @@ class App extends React.Component{
         })
         .then(response => response.json())
         .then(quote => {
-            this.setState({quotes: quote})
+            quote[0].content.length > 300
+             ? this.setState(prev => ({click: prev.click + 1}))
+             : this.setState({quotes: quote,loading: false})
         })
     }
 
@@ -41,16 +48,33 @@ class App extends React.Component{
         }))
     }
 
+    text(){
+        return document.getElementById('quote').textContent
+    }
+
     render(){
-        const { quotes } = this.state
+        const { quotes, click, loading } = this.state
+        if(loading)
+            return <div> loading </div>
+        const body = document.getElementById('body')
+        // console.log(document.getElementById('quote').textContent)
+        if(window.innerWidth <= 768){
+            click % 2 === 0
+            ? body.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.3)), url(${Moon}) `
+            : click % 3 === 0
+            ? body.style.backgroundImage = ` linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.3)), url(${Rocket})`
+            : body.style.backgroundImage = ` linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.3)), url(${Blood})`
+        }
+        
         return(
             <div>
-                <Navbar click={this.handleClick.bind(this)}/>
+                <Navbar click={this.handleClick}/>
                 {quotes && quotes.map(quote => (
                     <Quote 
                      key={quote.ID}
                      quote={quote.content}
-                     author={quote.title}   
+                     author={quote.title}  
+                     text={this.text.bind(this)} 
                     />
                 ))}
             </div>
