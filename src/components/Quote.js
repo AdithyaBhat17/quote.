372de39a6697340b5537ffb4ee3c091f3bf2d7f6
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-01-09 01:52:15 +0530

    ✨ share on whatsapp

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
 
-const Quote = ({quote, author}) => {
+library.add(faShareAlt)
+
+const Quote = ({quote, author, text}) => {
+    // console.log(document.getElementById('quote').textContent)
     return(
         <div>
-            <p dangerouslySetInnerHTML={{__html: quote}}>
-            </p>
+            {<p
+             id="quote"
+             dangerouslySetInnerHTML={{__html: quote}}>
+            </p>}
             <p>
                 <strong>{author}</strong>
             </p>
+            <a href={`whatsapp://send?text=${text}\n\n%2D%20%2A${author}`}>
+                <FontAwesomeIcon icon='share-alt' color='#fff'/>
+                &nbsp;whatsapp 
+            </a>
         </div>
     )
 }
